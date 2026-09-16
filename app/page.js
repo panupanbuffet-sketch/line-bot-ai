61b1969603ea48206d074c350fd030635e3de45f
panupanbuffet-sketch <panupan.buffet@gmail.com>
2026-09-16 13:16:38 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+      <p>
+            TASANA LINE Bot service is running. Webhook endpoint:{" "}
+                  <code>/api/line-webhook</code>
+                      </p>
+                        );
+                        }
+                        
